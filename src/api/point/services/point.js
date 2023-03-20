c6eab2ff28ce4f4ae62a6d9a400173e6c972fca8
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-03-19 17:03:57 -0700

    service to resolve points

--- src/api/point/services/point.js
+++ src/api/point/services/point.js
@@ -5,5 +5,17 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
+const axios = require('axios');
 
-module.exports = createCoreService('api::point.point');
+//'resolve' service:
+module.exports = createCoreService('api::point.point', ({ strapi }) => ({
+  async resolve(p) {
+    //create parameter string from ctx.query
+    const query = `lat=${p.x}&lon=${p.y}&height=100&date_from=2019-01-01&date_to=2019-12-31&&mean=year`
+    const { data } = await axios.get('http://windatlas.xyz/api/wind?' + query)
+    //parse csv data 
+    const s = parseFloat(data.split('\n')[3].split(',')[1])
+    //update p to have speed equal to s
+    await strapi.entityService.update('api::point.point', p.id, { data: { speed: s, status: 'RESOLVED' } })
+  }
+}));
